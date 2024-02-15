1546625163765a87092de51b6dd4434c41ffb83b
feiloo <105425286+feiloo@users.noreply.github.com>
2024-02-15 18:31:01 +0100

    add jobq

--- jobq/src/jobq/main.py
+++ jobq/src/jobq/main.py
@@ -2,8 +2,9 @@ import click
 import subprocess
 from time import sleep
 from pathlib import Path
+import sys
 
-def main(pid, cmd, verbose):
+def main(pid: int, cmd: str, verbose: bool) -> None:
     print(f'waiting for process {pid} and then run {cmd}')
     p = (Path(f'/proc/') / str(pid))
     if not p.exists():
@@ -12,22 +13,22 @@ def main(pid, cmd, verbose):
         sleep(1)
 
     proc = subprocess.run(cmd, text=True, shell=True, check=True, capture_output=True)
-    print(f'retcode: {proc.returncode}')
-    print(f'stderr: {proc.stderr}')
-    print(f'stdout:\n{proc.stdout}')
+    print(str(proc.stderr), file=sys.stderr)
+    print(str(proc.stdout), file=sys.stdout)
 
 
 # test
 # the & already separates the jobs
 # sleep 10 & jobq $! 'echo hello'
 
-
-
 @click.command()
 @click.argument('pid', type=int)
 @click.argument('cmd', type=str)
 @click.option('-v', '--verbose', 'verbose', type=bool, is_flag=True, default=False)
 def cli(*args, **kwargs):
+    '''
+    jobq runs a cmd after a process with pid has finished (regardless of sucess)
+    '''
     main(*args, **kwargs)
 
 
